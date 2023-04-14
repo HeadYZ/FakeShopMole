25931835d3282ca5d29e78d616a035b8664af4d1
Patryk <krasowski.p@outlook.com>
2023-04-14 21:46:50 +0200

    header slider data

---
+++ src/components/Header/headerData.js
@@ -0,0 +1,25 @@
+import childBigImg from '../../assets/img/header/child-big.jpg'
+import kitchenSmallImg from '../../assets/img/header/kitchen-big.jpg'
+import grilBigImg from  '../../assets/img/header/girl-big.jpg'
+
+const headerData = [
+	{
+		id: 1,
+		img: childBigImg,
+		alt: 'Zdjecie przedstawiające zbliżenie na przód aparatu fotograficznego, który trzyma dziewczynka.',
+		text: 'Skorzystaj z promocji -20% na aparaty fotograficzne.',
+	},
+	{
+		id: 2,
+		img: grilBigImg,
+		text: 'FRESH okazje! Sprawdź wiosenne promocje.',
+	},
+	{
+		id: 3,
+		img: kitchenSmallImg,
+		alt: 'Zdjecie przedstawiające nowoczesną kuchnie w tonacji drewna i szarego koloru.',
+		text: 'Potrzebna Ci pomoc przy zaprojektowaniu kuchni? Skontaktuj się z nami !',
+	},
+]
+
+export default headerData
