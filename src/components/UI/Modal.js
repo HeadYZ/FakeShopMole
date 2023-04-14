15f88ac9b581f69d05842ff65c5e51dd01a52a0e
Patryk <krasowski.p@outlook.com>
2023-04-14 21:42:33 +0200

    created reusable modal component

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,24 @@
+import { Fragment } from 'react'
+import ReactDOM from 'react-dom'
+import classes from './Modal.module.css'
+
+const Backdrop = props => {
+	return <div className={classes.backdrop} onClick={props.onClick}></div>
+}
+
+const ModalOverlay = props => {
+	return <div className={classes.modal}>{props.children}</div>
+}
+
+const portalElement = document.getElementById('overlays')
+
+const Modal = props => {
+	return (
+		<Fragment>
+			{ReactDOM.createPortal(<Backdrop onClick={props.onClick}></Backdrop>, portalElement)}
+			{ReactDOM.createPortal(<ModalOverlay onClick={props.onClick}>{props.children}</ModalOverlay>, portalElement)}
+		</Fragment>
+	)
+}
+
+export default Modal
