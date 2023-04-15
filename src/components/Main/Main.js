5ee4fad9b5940a727249c7cf162d77018580c7bf
Patryk <krasowski.p@outlook.com>
2023-04-15 19:51:29 +0200

    component with main offer

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import Wrapper from '../UI/Wrapper'
+import Offer from './Offers/Offer'
+
+const Main = props => {
+	return (
+		<main>
+			<Wrapper>
+				<Offer></Offer>
+			</Wrapper>
+		</main>
+	)
+}
+
+export default Main
