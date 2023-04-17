bd4ac53b8d419a26511d91a0820496567b7dbd4c
Patryk <krasowski.p@outlook.com>
2023-04-17 21:43:51 +0200

    converting text to a number in productPrice, transfer productUd and priceFor products in addItem function

--- src/components/Main/Offers/OfferSummary.js
+++ src/components/Main/Offers/OfferSummary.js
@@ -1,24 +1,31 @@
 import classes from './OfferSummary.module.css'
 import Modal from '../../UI/Modal'
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
 import CartContext from '../../store/cart-context'
 
 const OfferSummary = props => {
+	const productAmountRef = useRef()
+	const cartCtx = useContext(CartContext)
 	const productImg = props.onSelectedProduct.img
 	const productImgAlt = props.onSelectedProduct.alt
 	const productName = props.onSelectedProduct.nameOfEquipment
 	const productPrice = props.onSelectedProduct.price
-
-	const cartCtx = useContext(CartContext)
+	const productId = props.onSelectedProduct.id
 
 	const submitHandler = e => {
-		const selectedProduct = {
+		const productAmount = productAmountRef.current.value
+		const productAmountValue = +productAmount
+		const priceForProducts = +productPrice * productAmountValue
+
+		cartCtx.addItem({
 			productName: productName,
-			productPrice: productPrice,
-		}
-
-		cartCtx.addItem(selectedProduct)
-
+			productPrice: +productPrice,
+			productAmount: productAmountValue,
+			productImg: productImg,
+			productImgAlt: productImgAlt,
+			productId: productId,
+			priceForProducts: priceForProducts,
+		})
 		props.onClick()
 	}
 
@@ -32,7 +39,7 @@ const OfferSummary = props => {
 					<p className={classes['modal__product-name']}>{productName}</p>
 					<div className={classes['modal__box-price']}>
 						<p className={classes['modal__product-price']}>{productPrice} zł</p>
-						<input type='number' min='1' max='5' step='1' defaultValue='1'></input>
+						<input type='number' min='1' max='5' step='1' defaultValue='1' ref={productAmountRef}></input>
 					</div>
 				</div>
 			</div>
